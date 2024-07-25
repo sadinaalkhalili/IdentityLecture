839ef39dcbe9072ad95a99cdd0d13073dca0072a
sadinaalkhalili <sadinaalkhalili@hotmail.com>
2024-07-25 22:38:03 +0300

    first commit

---
+++ IdentityLecture2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityLecture2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ IdentityLecture2/Models/ViewModel/UserRegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityLecture2.Models.ViewModel
+{
+    public class UserRegisterViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
